3dcb68dc78bebbbc97b7dc5fde04aa4b9063305e
Reli <naitvenefy@gmail.com>
2024-10-25 10:02:49 -0600

    set cattpuccin theme

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps herecatppuccin-latte
+local keymaps = vim.keymap
+local opts = { noremap = true, silent = true }
